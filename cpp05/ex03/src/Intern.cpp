540c4326c5fd870d3c6a6ad78074128e83dae29a
MaximWDK <maxim.leonet@gmail.com>
2025-02-25 20:01:23 +0100

    Fix to avoid if elseif elseif else

--- cpp05/ex03/src/Intern.cpp
+++ cpp05/ex03/src/Intern.cpp
@@ -25,14 +25,23 @@ Intern::~Intern()
 
 AForm *Intern::makeForm(const std::string &name, const std::string &target)
 {
-	if (name == "ShrubberyCreationForm")
-		return new ShrubberyCreationForm(target);
-	else if (name == "RobotomyRequestForm")
-		return new RobotomyRequestForm(target);
-	else if (name == "PresidentialPardonForm")
-		return new PresidentialPardonForm(target);
-	else
-		throw FormNotFoundException();
+	std::string forms[3] = {"PresidentialPardonForm", "RobotomyRequestForm", "ShrubberyCreationForm"};
+	int i = 0;
+	
+	while (i < 3 && forms[i] != name)
+		i++;
+
+	switch (i)
+	{
+		case 0:
+			return new PresidentialPardonForm(target);
+		case 1:
+			return new RobotomyRequestForm(target);
+		case 2:
+			return new ShrubberyCreationForm(target);
+		default:
+			throw FormNotFoundException();
+	}
 }
 
 const char *Intern::FormNotFoundException::what() const throw()
